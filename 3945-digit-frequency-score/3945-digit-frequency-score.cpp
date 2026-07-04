d113931abb27ec5036dca18a657860a23be6435a
Deepanshu Rawat <141048115+deepanshuXrawat@users.noreply.github.com>
2026-07-04 22:27:50 +0530

    Time: 0 ms (100%), Space: 8.5 MB (95.24%) - LeetHub

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+        
+        int sum = 0;
+        while (n > 0){
+            int a = n % 10;
+            sum=sum+a;
+            n = n /10;
+        }
+
+        return sum;
+
+    }
+};
