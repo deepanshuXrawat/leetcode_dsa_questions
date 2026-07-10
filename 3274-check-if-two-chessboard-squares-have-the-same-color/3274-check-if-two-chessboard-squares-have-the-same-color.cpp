cab7293bbf674cf2573b507d4ade43b90b6c6c50
Deepanshu Rawat <141048115+deepanshuXrawat@users.noreply.github.com>
2026-07-10 22:17:06 +0530

    Time: 0 ms (100%), Space: 8 MB (59.5%) - LeetHub

---
+++ 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool checkTwoChessboards(string coordinate1, string coordinate2) {
+        
+        if( (coordinate1[0] + coordinate1[1] + coordinate2[0] + coordinate2[1]) % 2 == 0 ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
