1b0810d1136d25e74fcc2a993ecae10d2b00423d
Deepanshu Rawat <141048115+deepanshuXrawat@users.noreply.github.com>
2026-07-22 00:00:59 +0530

    Time: 0 ms (100%), Space: 7.8 MB (25.46%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string result = "";
+
+        while (columnNumber > 0) {
+            columnNumber--;
+
+            int remainder = columnNumber % 26;
+            result += ('A' + remainder);
+
+            columnNumber /= 26;
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
